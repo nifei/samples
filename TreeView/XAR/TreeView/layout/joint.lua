23da2bd10c363f9ed139ad608c2e24d12367551c
nifei <nieinifei@gmail.com>
2013-09-06 17:42:26 +0800

    clean

--- TreeView/XAR/TreeView/layout/joint.lua
+++ TreeView/XAR/TreeView/layout/joint.lua
@@ -7,12 +7,28 @@ function SetExpand(joint, expand)
 	if prevState ~= expand then joint:FireExtEvent("Triggered", expand) end
 end
 
+function SetIcons(joint, expandIcon, collapseIcon)
+	if expandIcon then
+		joint:GetAttribute().ExpandIcon = expandIcon
+	end
+	if collapseIcon then
+		joint:GetAttribute().CollapseIcon = collapseIcon
+	end
+end
+
 function IsExpand(joint)
 	return joint:GetAttribute().Expand
 end
 
 function OnLButtonUp(joint, x, y)
-	joint:SetExpand(not (joint:IsExpand()))
+	if joint:GetAttribute().ButtonDown == true then
+		joint:GetAttribute().ButtonDown = false
+		joint:SetExpand(not (joint:IsExpand()))
+	end
+end
+
+function OnLButtonDown(joint, x, y)
+	joint:GetAttribute().ButtonDown = true
 end
 
 function OnInitControl(joint)
